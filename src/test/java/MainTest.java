f3cc19b45d0398a8bb6812cb3b513812cd52c882
13855328587 <1978719543@qq.com>
2026-04-27 15:50:18 +0800

    test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testHello() {
+        String msg = "Hello, World!";
+        assertEquals("Hello, World!", msg);
+    }
+}
